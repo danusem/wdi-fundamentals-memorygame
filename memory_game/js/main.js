2d6849be1fca95b1168d4d2a1dbb3813b27ee5c6
Dan Usem <danusem@gmail.com>
2019-09-08 15:36:00 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,21 +1,26 @@
 var cards = ["queen", "queen", "king", "king"];
+cards.length;
 
 var cardsInPlay = [];
 
-var cardOne = cards[0]
-
-cardsInPlay.push('cardOne');
-
-console.log("User flipped queen");
-
-var cardTwo = cards[2]
-
-cardsInPlay.push('cardTwo');
-
-console.log("User flipped king");
-
-if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
-} else {
-	alert("Sorry, try again.");
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	console.log("You found a match!");
+	} else {
+	console.log("Sorry, try again.");
+	}
 }
+
+function flipCard(cardId) {
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+
+	if (cards.length === 2) {
+ 	checkForMatch();
+  	}
+
+}
+
+flipCard(0);
+flipCard(2);
+checkForMatch();
