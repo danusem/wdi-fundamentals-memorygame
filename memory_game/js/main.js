3450f400102e5beb655e53af87e1d87b0f042b52
Dan Usem <danusem@gmail.com>
2019-09-29 16:37:05 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,26 +23,36 @@ var cards = [
 
 var cardsInPlay = [];
 
+
 function checkForMatch() {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
-	} else {
-	alert("Sorry, try again.");
-	}
+	if (cardsInPlay.length === 2 )
+     if (cardsInPlay[0] === cardsInPlay[1]){
+        alert("You found a match!");
+    } else {
+        alert("Sorry try again!");
+    }
 }
 
-function flipCard(cardId) {
+function flipCard() {
+  var cardId = this.getAttribute('data-id');
   console.log("User flipped " + cards[cardId].rank);
   cardsInPlay.push(cards[cardId].rank);
+  this.setAttribute('src', cards[cardId].cardImage);
+  checkForMatch();
   console.log(cards[cardId].cardImage);
   console.log(cards[cardId].suit);
-
-	if (cards.length === 2) {
- 	checkForMatch();
-  	}
-
 }
 
-flipCard(0);
-flipCard(2);
-checkForMatch();
+function createBoard() {
+    for (var i = 0; i < cards.length; i++){
+        var cardElement = document.createElement('img');
+        cardElement.setAttribute('src','images/back.png');
+        cardElement.setAttribute('data-id', i);
+        document.getElementById('game-board').appendChild(cardElement);
+        cardElement.addEventListener('click', flipCard);
+
+
+}
+}
+
+createBoard();
